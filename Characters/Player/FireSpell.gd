923f961ed288d269fcda4c3d3e5b9296175e74b5
Dennis <dp.schmidt.95@gmail.com>
2019-03-22 14:03:43 +0100

    Fixed FireSpell animation

--- Characters/Player/FireSpell.gd
+++ Characters/Player/FireSpell.gd
@@ -1,6 +1,9 @@
 extends Particles2D
 
 onready var player = get_node("../..")
+onready var sprite = get_node("..")
+
+onready var positions = {'right': -position.x, 'left': position.x}
 
 
 func _ready():
@@ -17,8 +20,9 @@ func _cast_end():
 
 
 func _process(_delta):
-	if player.direction < 0:
-		rotation_degrees = 180
-	elif player.direction > 0:
-		rotation_degrees = 0
-	pass
+	if sprite.flip_h == true:
+		process_material.gravity = Vector3(-1000, 0, 0)
+		position.x = positions['right']
+	elif sprite.flip_h == false:
+		process_material.gravity = Vector3(1000, 0, 0)
+		position.x = positions['left']
