e0c695ffdc35df2b7cf8ca0e3a2f1704167036ab
Dennis <dp.schmidt.95@gmail.com>
2019-03-22 11:53:29 +0100

    Fixed StartMenu

--- Menus/StartMenu/StartMenu.gd
+++ Menus/StartMenu/StartMenu.gd
@@ -2,16 +2,10 @@ extends Control
 
 var temp
 
-func _ready():
-	pass # Replace with function body.
-
-
 
 func _on_Start_pressed():
-	temp = get_tree().change_scene("res://World.tscn")
-	pass # Replace with function body.
+	temp = get_tree().change_scene("res://Worlds/World.tscn")
 
 
 func _on_Quit_pressed():
 	temp = get_tree().quit()
-	pass # Replace with function body.
